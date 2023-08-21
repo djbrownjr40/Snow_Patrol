78e171d291634f522fce840994d3f6cca5c4b92d
Dennis Brown djbrownjr.40@gmail.com <djbrownjr40@gmail.com>
2023-08-21 17:58:30 +0900

    added validation

--- app/models/ski_resort.rb
+++ app/models/ski_resort.rb
@@ -3,4 +3,12 @@ class SkiResort < ApplicationRecord
   has_many :reviews, through: :check_ins
   has_many :snow_reports, through: :check_ins
   has_many :users, through: :check_ins
+
+  validates :name, presence: true
+  validates :location, presence: true
+  validates :description, presence: true
+  validates :average_rating, presence: true, inclusion: { in: 0..5 }
+  validates :url, presence: true
+  validates :latitude, presence: true, numericality: { greater_than_or_equal_to: -90.0, less_than_or_equal_to: 90.0 }
+  validates :longitude, presence: true, numericality: { greater_than_or_equal_to: -180.0, less_than_or_equal_to: 180.0 }
 end

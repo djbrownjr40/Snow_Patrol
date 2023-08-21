c10ea507ee4eb22272b7c250e5bef9040058a5d6
Naoki Takahashi <naokitakabusiness@gmail.com>
2023-08-21 17:54:53 +0900

    added validation to reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,4 @@
 class Review < ApplicationRecord
-  belongs_to :check_in
-
-  validates :comment, :waiting_rating, presence: true
+  validates :comment, presence: true, length: { minimum: 10, maximum: 500 }
+  validates :waiting_rating, presence: true, inclusion: { in: 0..5 }
 end

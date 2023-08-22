0901b524da76d30032b336330d20f84d4f2b616b
Dennis Brown djbrownjr.40@gmail.com <djbrownjr40@gmail.com>
2023-08-22 16:26:47 +0900

    map wip

--- app/controllers/resorts_controller.rb
+++ app/controllers/resorts_controller.rb
@@ -1,6 +1,13 @@
 class ResortsController < ApplicationController
   def index
-
+    @resorts = Resort.all
+    # The `geocoded` scope filters only resorts with coordinates
+    @markers = @resorts.geocoded.map do |resort|
+      {
+        lat: resort.latitude,
+        lng: resort.longitude
+      }
+    end
   end
 
   def show

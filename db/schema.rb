ce6eaa3abec5f129f1d50f0c66a5229831391e98
Noemi Radu <noemiradu12@gmail.com>
2023-08-23 15:36:06 +1000

    average snow report column added to ski resort

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_23_050905) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_23_052026) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -75,6 +75,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_23_050905) do
     t.float "longitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "average_snow_report"
   end
 
   create_table "snow_reports", force: :cascade do |t|

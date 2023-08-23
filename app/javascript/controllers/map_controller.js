2ee345ff07e2b3fd600cab78d9c7bad7e104e74c
Dennis Brown djbrownjr.40@gmail.com <djbrownjr40@gmail.com>
2023-08-23 15:42:02 +0900

    search functional

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,4 +1,5 @@
 import { Controller } from "@hotwired/stimulus"
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
 
 // Connects to data-controller="map"
 export default class extends Controller {
@@ -16,6 +17,8 @@ export default class extends Controller {
     });
   this.#addMarkersToMap()
   this.#fitMapToMarkers()
+  this.map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+    mapboxgl: mapboxgl }))
 }
 
   #addMarkersToMap() {

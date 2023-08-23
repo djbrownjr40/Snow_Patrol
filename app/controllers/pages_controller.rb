6f37fd9616b1822edcc13e50d832a38d02caa42a
Dennis Brown djbrownjr.40@gmail.com <djbrownjr40@gmail.com>
2023-08-23 13:01:56 +0900

    markers added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,15 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @ski_resorts = SkiResort.all
+    @markers = @ski_resorts.geocoded.map do |ski_resort|
+      {
+        lat: ski_resort.latitude,
+        lng: ski_resort.longitude
+      }
+    end
+
   end
+
 
 end

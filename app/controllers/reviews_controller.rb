ad23fb5453041b440310a1adb7083fe392cc7723
Naoki Takahashi <naokitakabusiness@gmail.com>
2023-08-25 10:33:39 +0900

    fix form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -12,7 +12,7 @@ class ReviewsController < ApplicationController
     @review = @check_in.reviews.build(reviews_params)
 
     if @review.save
-      redirect_to check_in_path(@check_in), notice: 'Your report have submitted successfully. Thank you!'
+      redirect_to check_in_path(@check_in), notice: 'Your review have submitted successfully! 🤙'
     else
       render 'new'
     end
@@ -23,4 +23,8 @@ class ReviewsController < ApplicationController
   def set_check_in
     @check_in = CheckIn.find(params[:check_in_id])
   end
+
+  # def review_params
+  #   params.require(:review).permit(:lift_wait_rating, :price_rating, :crowd_rating, :food_rating, :location_rating)
+  # end
 end

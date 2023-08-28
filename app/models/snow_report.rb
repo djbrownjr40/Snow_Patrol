9a02df173a20d57339dafbe7185d0449955c4d6b
Naoki Takahashi <naokitakabusiness@gmail.com>
2023-08-28 15:01:33 +0900

    fix form

--- app/models/snow_report.rb
+++ app/models/snow_report.rb
@@ -1,4 +1,4 @@
 class SnowReport < ApplicationRecord
   belongs_to :check_in
-  enum rating: %w[No_snow Slushy Icy Groomed_Snow Packed_Powder Pow]
+  enum rating: %w[no_snow slushy icy groomed_snow packed_powder pow]
 end

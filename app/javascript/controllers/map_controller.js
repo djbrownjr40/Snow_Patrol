849437b0e48508400d7f647d73b9c7abfb3062b7
Noemi Radu <noemiradu12@gmail.com>
2023-08-28 18:19:06 +1000

    scroling working - pins attached to cards - can't find the card that's on the screen - pin doesn't change yet

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,5 +1,5 @@
-import { Controller } from "@hotwired/stimulus"
-import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
+import { Controller } from "@hotwired/stimulus";
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
 
 // Connects to data-controller="map"
 export default class extends Controller {
@@ -8,21 +8,29 @@ export default class extends Controller {
     markers: Array
   }
 
+  static targets = ["map"]
+
   connect() {
+    console.log(this.mapTarget);
+    console.log(this.apiKeyValue);
     mapboxgl.accessToken = this.apiKeyValue;
     this.map = new mapboxgl.Map({
-      container: this.element, // container ID
+      container: this.mapTarget, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
     });
-  this.#addMarkersToMap()
-  this.#fitMapToMarkers()
-}
+    this.#addMarkersToMap()
+    this.#fitMapToMarkers()
+    this.#setupCardScrollListener(); // New line to set up scroll listener
+  }
 
   #addMarkersToMap() {
-    this.markersValue.forEach((marker) => {
-      new mapboxgl.Marker()
-        .setLngLat([ marker.lng, marker.lat ])
-        .addTo(this.map)
+    this.markersValue.forEach((markerData) => {
+      const marker = new mapboxgl.Marker()
+        .setLngLat([markerData.lng, markerData.lat])
+        .addTo(this.map);
+
+      // Attach the ski resort ID to the marker element
+      marker.getElement().setAttribute("data-ski-resort-id", markerData.marker_id);
     });
   }
 
@@ -30,5 +38,54 @@ export default class extends Controller {
     const bounds = new mapboxgl.LngLatBounds()
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
     this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 15 })
+  }
+
+  #findVisibleCard(cardsContainer) {
+    const cards = cardsContainer.querySelectorAll(".card");
+    const containerRect = cardsContainer.getBoundingClientRect();
+
+    for (const card of cards) {
+      const cardRect = card.getBoundingClientRect();
+
+      if (
+        cardRect.left >= containerRect.left &&
+        cardRect.right <= containerRect.right
+      ) {
+        return card; // Return the first visible card
+      }
+    }
+
+    return null; // No visible card found
+  }
+
+  updateMarkerColors(event) {
+    console.log(event);
+    const visibleCard = this.#findVisibleCard(event.target)
+    const skiResortId = visibleCard.getAttribute("data-ski-resort-id");
+
+    console.log(this.map);
+    this.markersValue.forEach(marker => {
+      const new_marker = this.map.getLayer(marker.id);
+      console.log(new_marker);
+      if (new_marker) {
+        if (marker.id === skiResortId) {
+          // Change marker color for the visible card
+          new_marker.getElement().style.color = "red";
+        } else {
+          // Reset color for other markers
+          new_marker.getElement().style.color = "blue";
+        }
+      }
+    });
+  }
+
+  #setupCardScrollListener() {
+    const cardsContainer = document.querySelector("[data-controller='map']");
+    cardsContainer.addEventListener("scroll", () => {
+      const visibleCard = this.#findVisibleCard(cardsContainer);
+      if (visibleCard) {
+        this.updateMarkerColors(visibleCard);
+      }
+    });
   }
 }

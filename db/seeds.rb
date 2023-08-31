88555cd0504d29cc186c2652e4fce62b1e2e76e8
George Arbanas <george.arbanas@icloud.com>
2023-08-31 13:48:13 +0900

    added more reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,88 +9,88 @@ require 'net/http'
 # SEED ONCE WITH THE RESORTS UNCOMENTED AND THEN COMMENT THEM OUT
 # there's no point on destroying and recreating them cause they're going to stay the same
 
-ski_resorts = 'db/ski_resorts.csv'
+# ski_resorts = 'db/ski_resorts.csv'
 
-puts 'Detroying all previous ski reports...'
-SkiResort.destroy_all
+# puts 'Detroying all previous ski reports...'
+# SkiResort.destroy_all
 
-CSV.foreach(ski_resorts, headers: :first_row, header_converters: :symbol) do |row|
-  puts 'creating ski resort'
-  ski_resort = SkiResort.create!(
-    {
-      name: row[:resort_name],
-      location: "#{row[:town]}, #{row[:prefecture]}",
-      description: "#{row[:name_ja]}, #{row[:address_ja]}",
-      url: "https://www.snowjapan.com/japan-ski-resorts" + row[:url_path],
-      latitude: row[:latitude],
-      longitude: row[:longitude],
-      height: rand(150.0..350.0),
-      length: rand(15.0..35.0),
-      temp: rand(-15.0..3.0),
-      features: {
-        restaurant: [true, false].sample,
-        restroom: [true, false].sample,
-        looker_room: [true, false].sample,
-        rental_wear: [true, false].sample,
-        shower_room: [true, false].sample,
-        english_friendly: [true, false].sample,
-        kids_friendly:[true, false].sample
-      },
-      courses: {
-        easy: rand(0..4),
-        intermediate: rand(0..4),
-        advanced: rand(0..4)
-      }
-    }
-  )
-  dashed_name = ski_resort.name.gsub(" ", "-")
-  url = "https://www.snowjapan.com/rest-api/skiarea/#{dashed_name}"
-  uri = URI(url)
-  body = {}
-  headers = {"accept": "application/json, text/plain, */*", "accept-language": "en-US,en;q=0.5"}
-  response = Net::HTTP.post(uri, body.to_json, headers)
-  resort = JSON.parse(response.body)
-  id = resort['Id']
+# CSV.foreach(ski_resorts, headers: :first_row, header_converters: :symbol) do |row|
+#   puts 'creating ski resort'
+#   ski_resort = SkiResort.create!(
+#     {
+#       name: row[:resort_name],
+#       location: "#{row[:town]}, #{row[:prefecture]}",
+#       description: "#{row[:name_ja]}, #{row[:address_ja]}",
+#       url: "https://www.snowjapan.com/japan-ski-resorts" + row[:url_path],
+#       latitude: row[:latitude],
+#       longitude: row[:longitude],
+#       height: rand(150.0..350.0),
+#       length: rand(15.0..35.0),
+#       temp: rand(-15.0..3.0),
+#       features: {
+#         restaurant: [true, false].sample,
+#         restroom: [true, false].sample,
+#         looker_room: [true, false].sample,
+#         rental_wear: [true, false].sample,
+#         shower_room: [true, false].sample,
+#         english_friendly: [true, false].sample,
+#         kids_friendly:[true, false].sample
+#       },
+#       courses: {
+#         easy: rand(0..4),
+#         intermediate: rand(0..4),
+#         advanced: rand(0..4)
+#       }
+#     }
+#   )
+#   dashed_name = ski_resort.name.gsub(" ", "-")
+#   url = "https://www.snowjapan.com/rest-api/skiarea/#{dashed_name}"
+#   uri = URI(url)
+#   body = {}
+#   headers = {"accept": "application/json, text/plain, */*", "accept-language": "en-US,en;q=0.5"}
+#   response = Net::HTTP.post(uri, body.to_json, headers)
+#   resort = JSON.parse(response.body)
+#   id = resort['Id']
 
-  photo_url = "https://www.snowjapan.com/rest-api/skiarea/photos/1"
-  photo_uri = URI(photo_url)
-  photo_body = {"resortid": id}
-  photo_headers = {
-    "accept": "application/json, text/plain, */*",
-    "content-type": "application/json;charset=UTF-8"
-  }
-  photo_response = Net::HTTP.post(photo_uri, photo_body.to_json, photo_headers)
-  parsed_photos = JSON.parse(photo_response.body)
-  p parsed_photos[0]
-  parsed_photo = parsed_photos[0]
-  if parsed_photo
+#   photo_url = "https://www.snowjapan.com/rest-api/skiarea/photos/1"
+#   photo_uri = URI(photo_url)
+#   photo_body = {"resortid": id}
+#   photo_headers = {
+#     "accept": "application/json, text/plain, */*",
+#     "content-type": "application/json;charset=UTF-8"
+#   }
+#   photo_response = Net::HTTP.post(photo_uri, photo_body.to_json, photo_headers)
+#   parsed_photos = JSON.parse(photo_response.body)
+#   p parsed_photos[0]
+#   parsed_photo = parsed_photos[0]
+#   if parsed_photo
 
-    parsed_photo = parsed_photos[0]
-    parsed_photo_name = parsed_photo["PhotoFileName"].gsub(' ', '%20')
-    parsed_photo_user = parsed_photo["CreatedByUserName"].gsub(' ', '%20')
+#     parsed_photo = parsed_photos[0]
+#     parsed_photo_name = parsed_photo["PhotoFileName"].gsub(' ', '%20')
+#     parsed_photo_user = parsed_photo["CreatedByUserName"].gsub(' ', '%20')
 
-    photo_file_url = "https://www.snowjapan.com/photo-galleries/#{parsed_photo_user}/#{parsed_photo_name}"
-    photo_file = URI.open(photo_file_url)
+#     photo_file_url = "https://www.snowjapan.com/photo-galleries/#{parsed_photo_user}/#{parsed_photo_name}"
+#     photo_file = URI.open(photo_file_url)
 
-    ski_resort.photo.attach(io: photo_file, filename: "resort.png", content_type: "image/png")
-  end
-end
-puts 'ski resorts made'
+#     ski_resort.photo.attach(io: photo_file, filename: "resort.png", content_type: "image/png")
+#   end
+# end
+# puts 'ski resorts made'
 
-puts 'Detroying all previous users...'
-User.destroy_all
-puts 'Detroying all previous check_ins...'
-CheckIn.destroy_all
-puts 'Detroying all previous reviews...'
-Review.destroy_all
-puts 'Detroying all previous snow reports...'
-SnowReport.destroy_all
+# puts 'Detroying all previous users...'
+# User.destroy_all
+# puts 'Detroying all previous check_ins...'
+# CheckIn.destroy_all
+# puts 'Detroying all previous reviews...'
+# Review.destroy_all
+# puts 'Detroying all previous snow reports...'
+# SnowReport.destroy_all
 
-puts 'Creating now a new db!'
+# puts 'Creating now a new db!'
 
 # creating 10 users, each of them has been to 5 resorts, left 1 review and 1 snow report
 # none of them have a default resort assigned
-10.times do
+100.times do
   user = User.create!(
     {
       email: Faker::Internet.email,
@@ -103,8 +103,8 @@ puts 'Creating now a new db!'
     }
   )
 
-  5.times do
-    check_in = CheckIn.new({ checked_out_at: Faker::Date.between(from: 1095.days.ago, to: Date.today) })
+  50.times do
+    check_in = CheckIn.new({ checked_out_at: Faker::Time.between_dates(from: Date.today - 1, to: Date.today, period: :all) })
     check_in.user = user
     check_in.ski_resort = SkiResort.all.sample
     check_in.save!
